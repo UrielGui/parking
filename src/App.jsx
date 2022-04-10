4e3ebe421da7110ba9eafad9aec9b842b17198f6
Uriel Guimarães <urieligara@hotmail.com>
2022-04-10 04:23:14 -0300

    GlobalStates added

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import * as Styled from './Styles/Globals';
 import MenuContainer from './Components/MenuContainer';
 import InputComponent from './Components/Input';
 import OutputComponent from './Components/Output';
+import GlobalStates from './GlobalStates';
 
 export default function App() {
   const [show, setShow] = useState('InputComponent');
+
   return (
     <div className="App">
-      <Styled.MainContainer>
-        <MenuContainer show={show} setShow={setShow} />
-        <Styled.Container>
-          {show === 'InputComponent' ? <InputComponent /> : <OutputComponent />}
-        </Styled.Container>
-      </Styled.MainContainer>
+      <GlobalStates>
+        <Styled.MainContainer>
+          <MenuContainer show={show} setShow={setShow} />
+          <Styled.Container>
+            {show === 'InputComponent' ? <InputComponent /> : <OutputComponent />}
+          </Styled.Container>
+        </Styled.MainContainer>
+      </GlobalStates>
     </div>
   );
 }
