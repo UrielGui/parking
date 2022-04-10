dea8915eaf384618c92648f5e1152c2551da42eb
Uriel Guimarães <urieligara@hotmail.com>
2022-04-10 14:53:31 -0300

    GlobalStates updated

--- src/GlobalStates/index.jsx
+++ src/GlobalStates/index.jsx
@@ -2,13 +2,25 @@ import React, {useState} from 'react';
 import { PlateInfo } from '../Contexts';
 
 export default function GlobalStates({ children }) {
-  const [reservation, setReservation] = useState('teste');
+  const [reservation, setReservation] = useState(null);
+  const [paymentRecord, setPaymentRecord] = useState(null);
+  const [paymentRecordStatus, setPaymentRecordStatus] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [paymentRecordErrorMsg, setPaymentRecordErrorMsg] = useState(null)
   return (
     <>
       <PlateInfo.Provider
         value={{
           reservation,
-          setReservation
+          setReservation,
+          paymentRecord,
+          setPaymentRecord,
+          loading,
+          setLoading,
+          paymentRecordStatus,
+          setPaymentRecordStatus,
+          paymentRecordErrorMsg,
+          setPaymentRecordErrorMsg
         }}
       >
         {children}
