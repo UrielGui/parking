31947834b54b5ce92c84c4be34705fb7b169dd27
Uriel Guimarães <urieligara@hotmail.com>
2022-04-11 20:25:56 -0300

    PlateNumberPost updated

---
+++ src/Services/PlateNumberPost.service.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export function PlateNumberPost(plateNumber, setLoading, setReservation, setErrorOrSuccess, setErrorMsg) {
+  axios.post('https://parking-lot-to-pfz.herokuapp.com/parking', {
+    plate: plateNumber
+  }).then((response) => {
+      setReservation(response.data.reservation)
+      setErrorOrSuccess('success')
+      setLoading(false)
+    }).catch((error) => {
+      setErrorOrSuccess('error')
+      setLoading(false)
+      if(error.response.data.errors.plate.find(e => e === "is invalid")) {
+        return setErrorMsg('Placa inválida!')
+      }
+      else if(error.response.data.errors.plate.find(e => e === "already parked")) {
+        return setErrorMsg('Veículo já estacionado!')
+      }
+      else {
+        return setErrorMsg('Serviço indisponível no momento!')
+      }
+    });
+}
